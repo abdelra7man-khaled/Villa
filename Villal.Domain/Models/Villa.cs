7614d03ed52db6b33f6e008da984fb09f87c39ad
Abdelrahman <abdokhm13@gmail.com>
2026-02-11 21:30:06 +0200

    update labels of Price and ImageUrl in Create villa UI

--- Villal.Domain/Models/Villa.cs
+++ Villal.Domain/Models/Villa.cs
@@ -1,13 +1,17 @@
-﻿namespace Villal.Domain.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Villal.Domain.Models
 {
     public class Villa
     {
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name = "Price per Night")]
         public decimal Price { get; set; }
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
+        [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
