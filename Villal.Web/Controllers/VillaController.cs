c1bf4472c182cc17d903720a771e4adb70eff0c8
Abdelrahman <abdokhm13@gmail.com>
2026-02-11 20:41:27 +0200

    Add Villa Controller with index action to display all villas

---
+++ Villal.Web/Controllers/VillaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Villal.Infrastructure.Data;
+
+namespace Villal.Web.Controllers
+{
+    public class VillaController(AppDbContext _context) : Controller
+    {
+        public IActionResult Index()
+        {
+            var villas = _context.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
