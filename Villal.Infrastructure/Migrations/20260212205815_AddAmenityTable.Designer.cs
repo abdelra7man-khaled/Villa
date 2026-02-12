989adf311f86ffebd5ecb1d5b7c0c6351fbc6d9d
Abdelrahman <abdokhm13@gmail.com>
2026-02-12 22:59:40 +0200

    Add Amenity Entity and Seed its Data to DB

--- Villal.Infrastructure/Migrations/20260212205815_AddAmenityTable.Designer.cs
+++ Villal.Infrastructure/Migrations/20260212205815_AddAmenityTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Villal.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using Villal.Infrastructure.Data;
 namespace Villal.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260212205815_AddAmenityTable")]
+    partial class AddAmenityTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
