ab7d0155ccb51223c378468de87111e65e73d3f1
Abdelrahman <abdokhm13@gmail.com>
2026-02-12 19:01:42 +0200

    Add Villa Number Controller with Index and Create Functionality

--- Villal.Domain/Entities/VillaNumber.cs
+++ Villal.Domain/Entities/VillaNumber.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Villal.Domain.Entities
@@ -6,9 +7,11 @@ namespace Villal.Domain.Entities
     public class VillaNumber
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Villa Number")]
         public int VillaNo { get; set; }
         [ForeignKey("Villa")]
         public int VillaId { get; set; }
+        [ValidateNever]
         public Villa Villa { get; set; }
         public string? SpecialDetails { get; set; }
     }
---
+++ Villal.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Villal.Domain.Entities;
+using Villal.Infrastructure.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Villal.Web.Controllers
+{
+    public class VillaNumberController(AppDbContext _context) : Controller
+    {
+        public IActionResult Index()
+        {
+            var villaNumbers = _context.VillaNumbers.ToList();
+            return View(villaNumbers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VillaNumber newVillaNumber)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.VillaNumbers.Add(newVillaNumber);
+                _context.SaveChanges();
+
+                TempData["success"] = "Villa Number created successfully";
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["error"] = "Failed to create new villa Number";
+
+            return View(newVillaNumber);
+        }
+
+        public IActionResult Update(int? id)
+        {
+            var villaToUpdate = _context.Villas.FirstOrDefault(v => v.Id == id);
+            if (villaToUpdate == null)
+            {
+                return RedirectToAction(nameof(Error), "Home");
+            }
+
+            return View(villaToUpdate);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa villaToUpdate)
+        {
+            if (ModelState.IsValid && villaToUpdate.Id > 0)
+            {
+                _context.Villas.Update(villaToUpdate);
+                _context.SaveChanges();
+
+                TempData["success"] = "Villa updated successfully";
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["error"] = "Failed to update villa";
+
+            return View(villaToUpdate);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            var villaToDelete = _context.Villas.FirstOrDefault(v => v.Id == id);
+            if (villaToDelete == null)
+            {
+                return RedirectToAction(nameof(Error), "Home");
+            }
+
+            return View(villaToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa villaToDelete)
+        {
+            var existingVilla = _context.Villas.FirstOrDefault(v => v.Id == villaToDelete.Id);
+
+            if (existingVilla is not null)
+            {
+                _context.Villas.Remove(existingVilla);
+                _context.SaveChanges();
+
+                TempData["success"] = "Villa deleted successfully";
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["error"] = "Failed to delete villa";
+
+            return View(existingVilla);
+        }
+    }
+}
