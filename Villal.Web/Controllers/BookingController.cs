15fd1173c1365d80f7768882b7afc090c514a804
Abdelrahman <abdokhm13@gmail.com>
2026-02-20 01:50:38 +0200

    Add Booking Controller and FinalizeBooking Action

---
+++ Villal.Web/Controllers/BookingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Villal.Application.Common.Interfaces;
+using Villal.Domain.Entities;
+
+namespace Villal.Web.Controllers
+{
+    public class BookingController(IUnitOfWork unitOfWork) : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        public async Task<IActionResult> FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
+        {
+            Booking booking = new()
+            {
+                VillaId = villaId,
+                CheckInDate = checkInDate,
+                CheckOutDate = checkInDate.AddDays(nights),
+                Nights = nights,
+                Villa = await _unitOfWork.Villa.GetAsync(v => v.Id == villaId, includeProperties: "VillaAmenity")
+            };
+            return View(booking);
+        }
+    }
+}
