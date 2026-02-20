7466d249f43919c69cf2b8ed145d44269cda8b8a
Abdelrahman <abdokhm13@gmail.com>
2026-02-20 02:24:04 +0200

    Add Authorization to FinalizeBooking and get user details

--- Villal.Web/Controllers/BookingController.cs
+++ Villal.Web/Controllers/BookingController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using Villal.Application.Common.Interfaces;
 using Villal.Domain.Entities;
 
@@ -8,15 +10,27 @@ namespace Villal.Web.Controllers
     {
         private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
+
+        [Authorize]
         public async Task<IActionResult> FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
         {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            var userId = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+
+            ApplicationUser appUser = await _unitOfWork.ApplicationUser.GetAsync(u => u.Id == userId);
             Booking booking = new()
             {
                 VillaId = villaId,
                 CheckInDate = checkInDate,
                 CheckOutDate = checkInDate.AddDays(nights),
                 Nights = nights,
-                Villa = await _unitOfWork.Villa.GetAsync(v => v.Id == villaId, includeProperties: "VillaAmenity")
+                Villa = await _unitOfWork.Villa.GetAsync(v => v.Id == villaId, includeProperties: "VillaAmenity"),
+                UserId = userId ?? string.Empty,
+                User = appUser,
+                Phone = appUser.PhoneNumber,
+                Email = appUser.Email ?? string.Empty,
+                Name = appUser?.Name ?? string.Empty
             };
             booking.TotalPrice = booking.Villa.Price * nights;
 
